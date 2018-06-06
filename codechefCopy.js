2c2daad5fa2f7cb1d17794ce3e8dd8d51af7f9fc
MajAK <majmundarkushal9@gmail.com>
2018-06-06 14:29:04 +0530

    File added

---
+++ codechefCopy.js
@@ -0,0 +1,46 @@
+var copyToClipboard = function(str)
+{
+  var el = document.createElement('textarea');
+  el.value = str;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  const selected =
+    document.getSelection().rangeCount > 0
+      ? document.getSelection().getRangeAt(0)
+      : false;
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  if (selected) {
+    document.getSelection().removeAllRanges();
+    document.getSelection().addRange(selected);
+  }
+};
+var copy_str="";
+var req = document.getElementsByTagName("pre")[0];
+if(req!=undefined)
+{
+  if(req.childElementCount==1)
+  {   var child=(req.children)[0];
+      copy_str=child.innerHTML;
+  }
+  else
+  {
+      var temp_str=req.innerHTML;
+      var idx=0;
+      while(temp_str[idx]!='>')
+        idx++;
+      idx++;
+      while(temp_str[idx]!='>')
+        idx++;
+      idx+=2;
+      while(temp_str[idx]!='<')
+      {
+        copy_str+=temp_str[idx];
+        idx++;
+      }
+  }
+  req.innerHTML="<button class='button blue right' onclick='copyToClipboard(copy_str)'>Copy</button>"+req.innerHTML;
+}
